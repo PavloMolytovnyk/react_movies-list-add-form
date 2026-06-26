548c7f07f6e40ea38cb9267e80980704941ec0d6
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-06-26 13:37:34 +0300

    Change 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+import { useState } from 'react';
 
 export const App = () => {
+  const [posts, setPosts] = useState(moviesFromServer);
+
+  const handlePost = (movie: Movie) => {
+    setPosts(currentPost => [...currentPost, movie]
+    );
+  }
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={posts} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handlePost}/>
       </div>
     </div>
   );
